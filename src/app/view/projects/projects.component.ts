013c69003052ee604fdd6139a69a68459d80b2fa
Dani <daarro@floridauniversitaria.es>
2025-06-02 09:37:25 +0200

    Mejorando logica projects

--- src/app/view/projects/projects.component.ts
+++ src/app/view/projects/projects.component.ts
@@ -13,5 +13,31 @@ export class ProjectsComponent {
 
   public showHiddenCard(project: string): void{
     this.showProjectCard = project;
+    this.moveToHiddenCard(project);
   }
+
+public moveToHiddenCard(project: string): void {
+  let cardID = "";
+
+  if (project === "StockMaster") {
+    cardID = "StockMasterInfo";
+  } else if (project === "AliciaEstetica") {
+    cardID = "AliciaEsteticaInfo";
+  } else {
+    cardID = "EcoBuddyInfo";
+  }
+
+  const tryScroll = (retries = 10) => {
+    let element = document.getElementById(cardID);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    } else if (retries > 0) {
+      setTimeout(() => tryScroll(retries - 1), 200); 
+    }
+  };
+
+  tryScroll();
+}
+
+
 }
