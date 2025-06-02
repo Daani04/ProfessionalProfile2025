5bc39cb7d53e706a1b6fd599a2391559477dca73
Dani <daarro@floridauniversitaria.es>
2025-06-02 15:02:11 +0200

    Implementando funcion cambio idioma(HAY QUE MODIFICARLA)

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Router } from '@angular/router';
+import { LenguajeServiceService } from '../../services/translate.service';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +11,38 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
 
-  constructor(private router: Router) {}
+  public isSpanish: boolean = false;
+
+    constructor(
+      private router: Router,
+      private languageService:LenguajeServiceService ,
+ 
+    )
+      {
+      this.languageService.isSpanish$.subscribe(
+        (isSpanish: boolean) => this.isSpanish = isSpanish
+      );
+    }
+ 
+ 
+    toggleLanguage(language: 'es' | 'en') {
+      this.languageService.setLanguage(language);
+      localStorage.setItem('language', language);
+     
+    }
+     getText(es: string, en: string): string {
+    return this.isSpanish ? es : en;
+  }
 
   public goHomePage(): void {
      this.router.navigate(['/home']);
+  }
+
+  public changueLenguage(lenguege: string): void {
+    if (lenguege === "es") {
+      this.isSpanish = true;
+    } else {
+      this.isSpanish = false;
+    }
   }
 }
--- src/app/component/home-header/home-header.component.ts
+++ src/app/component/home-header/home-header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LenguajeServiceService } from '../../services/translate.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-header',
@@ -8,5 +10,48 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './home-header.component.css'
 })
 export class HomeHeaderComponent {
+  public isSpanish: boolean = false;
+
+    constructor(
+      private router: Router,
+      private languageService:LenguajeServiceService ,
+ 
+    )
+      {
+      this.languageService.isSpanish$.subscribe(
+        (isSpanish: boolean) => this.isSpanish = isSpanish
+      );
+    }
+ 
+ 
+    toggleLanguage(language: 'es' | 'en') {
+      this.languageService.setLanguage(language);
+      localStorage.setItem('language', language);
+    }
+     getText(es: string, en: string): string {
+    return this.isSpanish ? es : en;
+  }
+
+  public goHomePage(): void {
+     this.router.navigate(['/home']);
+  }
+
+  public changueLenguage(lenguege: string): void {
+    if (lenguege === "es") {
+      this.isSpanish = true;
+      localStorage.setItem('language', 'es');
+    } else {
+      this.isSpanish = false;
+      localStorage.setItem('language', 'en');
+    }
+  }
+
+ngOnInit() {
+  if (localStorage.getItem('lenguage') === 'es') {
+    this.isSpanish = true;
+  } else {
+    this.isSpanish = false;
+  }
+}
 
 }
