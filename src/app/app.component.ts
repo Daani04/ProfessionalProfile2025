9aeb5f5cacf831d8f3e73ee36d897342980642bb
Dani <daarro@floridauniversitaria.es>
2025-06-02 14:10:33 +0200

    Cambiando botones y añadiendo link a github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,13 @@ import { HomeComponent } from "./view/home/home.component";
 import { HeaderComponent } from "./component/header/header.component";
 import { trigger, transition, style, animate, query, group } from '@angular/animations';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 
 @Component({
   selector: 'app-root',
--- src/app/view/about-me/about-me.component.ts
+++ src/app/view/about-me/about-me.component.ts
@@ -8,7 +8,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-about-me',
-  imports: [HeaderComponent, ContactComponent],
+  imports: [HeaderComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
